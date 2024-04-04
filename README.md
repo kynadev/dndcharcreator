6cd163d4dc2e0b30631fb1656d68c007c5e992e2
Kyna Develle <kyna.osprey@outlook.com>
2024-04-03 19:40:27 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+### SvelteKit and Tauri App
+This shows how to set up a [Tauri-SvelteKit App](https://tauri.app/v1/guides/getting-started/setup/sveltekit/).
+
+To start the dev build of the app run
+```
+$ npm run tauri dev
+```
---
+++ src/routes/+layout.ts
@@ -0,0 +1,8 @@
+/**
+ * Enables prerendering in accordance with Tauri SvelteKit Guide
+ */
+export const prerender: boolean = true
+/**
+ * Disables SSR in accordance with Tauri SvelteKit Guide
+ */
+export const ssr: boolean = false
