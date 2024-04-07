c77090970fbcb8c9869c606f914cc922112010b1
Kyna Develle <kyna.osprey@outlook.com>
2024-04-07 15:41:03 -0700

    Adds Starting Linting standards

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,37 @@ module.exports = {
 		'prettier'
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', '@stylistic/js/indent'],
+	rules: {
+		eqeqeq: ['warn', 'smart'],
+		'@stylistic/js/indent': 'warn',
+		'@stylistic/js/no-mixed-spaces-and-tabs': ['warn', 'smart-tabs'],
+		'@stylistic/js/quote-props': ['warn', 'as-needed', { keywords: true }],
+		'@stylistic/js/quotes': ['warn', 'single', { avoidEscape: true }],
+		'@stylistic/js/comma-dangle': ['warn', 'always-multiline'],
+		'@stylistic/js/semi': [
+			'error',
+			'never',
+			{
+				omitLastInOneLineBlock: true
+			}
+		],
+		'@stylistic/js/semi-style': ['error', 'last'],
+		'@stylistic/js/no-extra-semi': 'error',
+		'@stylistic/js/comma-spacing': ['warn', { before: false, after: true }],
+		'@stylistic/js/dot-location': ['warn', 'property'],
+		'@stylistic/js/eol-last': ['warn', 'always'],
+		'@stylistic/js/spaced-comment': ['error', 'always', { exceptions: ['*'] }],
+		'@stylistic/js/space-infix-ops': ['error', { int32Hint: false }],
+		'@stylistic/js/no-trailing-spaces': [
+			'warn',
+			{
+				skipBlankLines: true,
+				ignoreComments: true
+			}
+		],
+		'@stylistic/js/no-mixed-operators': ['warn', 'always']
+	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
