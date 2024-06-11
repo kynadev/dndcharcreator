f12fa2e81941ffc98db33980208b3c3fa970cdca
Kyna <kyna.osprey@outlook.com>
2024-06-10 21:48:04 -0700

    stuff

---
+++ src/lib/Box.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	type BoxType = 'default' | '';
+</script>
+
+<div class="s">
+	Incididunt tempor ipsum minim nulla labore cillum irure. Consectetur duis sunt Lorem do do. Sit
+	duis ex Lorem consequat eu proident est dolor consectetur Lorem ex magna dolore. Dolore proident
+	quis nostrud voluptate laboris sint mollit quis eiusmod eiusmod ea. Elit mollit consectetur sunt
+	esse commodo minim cillum Lorem fugiat officia adipisicing eiusmod. Pariatur sunt ut est aliqua
+	dolor ipsum. Velit consequat incididunt voluptate non in. Commodo commodo velit enim enim tempor
+	eiusmod aliquip voluptate non enim labore elit. In culpa culpa ullamco est. Officia consequat
+	labore cupidatat consequat excepteur quis minim quis ea incididunt consequat.
+</div>
+
+<style>
+	div {
+		padding: 10px;
+		border-image-slice: 20 20 20 20;
+		border-image-width: 20px 20px 20px 20px;
+		/* border-image-outset: 10px 0px 0px 0px; */
+		border-image-repeat: stretch stretch;
+		border-image-source: url('/box_border.svg');
+	}
+	/* TODO add options here for different types of boxes based on classes */
+</style>
---
+++ src/lib/Greeter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+</script>
